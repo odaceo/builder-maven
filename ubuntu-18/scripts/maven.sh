169959e9c626c23865e3341522d8afc463f7e967
Alexandre GARINO <alexandre.garino@odaceo.ch>
2018-06-17 18:30:31 +0200

    Polish comments

--- ubuntu-18/scripts/maven.sh
+++ ubuntu-18/scripts/maven.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# This install script is intended to install Maven.
+#
+# Environment variables:
+# - MAVEN_VERSION: the version to install
+
 # Update your local package index
 apt-get update
 
-# Install PIP package
+# Install Maven package
 apt-get install -y maven=${MAVEN_VERSION}.*
 
 # Remove the local package index
 rm -rf /var/lib/apt/lists/*
 
-# Print the installed Gradle version
+# Print the installed Maven version
 mvn --version
